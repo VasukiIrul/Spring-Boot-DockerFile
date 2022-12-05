61040d7d91791141dc7bd04dd9642dae8d9067c0
vasuki <vasuki.tce@gmail.com>
2022-12-05 08:50:26 +0530

    first commit

---
+++ src/main/java/com/Demo/DockerSpringBootDemo1Application.java
@@ -0,0 +1,13 @@
+package com.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringBootDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringBootDemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Demo/DockerSpringBootDemo1ApplicationTests.java
@@ -0,0 +1,20 @@
+package com.Demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest
+class DockerSpringBootDemo1ApplicationTests {
+	@Autowired
+	WelcomeController controller;
+
+	@Test
+	public void addTest() {
+		ResponseEntity<Integer> result = controller.addTwoValues(20, 12);
+		Assertions.assertEquals(result.getBody(), 32);
+	}
+
+}
